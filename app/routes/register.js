4c98d12730a41959c450700908b6a92cbbb18c0f
Mackenzie Craig <maccraig98@gmail.com>
2018-03-19 14:16:47 -0700

    Make db query async

--- app/routes/register.js
+++ app/routes/register.js
@@ -14,51 +14,56 @@ router.post('/api/register', function(req, res) {
   var password = req.body.password;
 
   // make sure user doesn't already exist
-  User.findOne({
-    name: name
-  }, function(err, user) {
+  async function getAThing() {
+    User.findOne({
+      name: name
+    }, function(err, user) {
 
-    if (err) {
-      throw err;
-    }
+      if (err) {
+        throw err;
+      }
 
-    if (user) {
-      res.json({
-        success: false,
-        message: 'Register failed. Someone already has this name.'
-      });
-    } else {
+      if (user) {
+        res.json({
+          success: false,
+          message: 'Register failed. Someone already has this name.'
+        });
+      } else {
 
-      bcrypt.genSalt(12, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-          var newUser = new User({
-            name: name,
-            password: hash,
-            admin: false,
-            shots_fired: parseInt(0),
-            kills: parseInt(0),
-            deaths: parseInt(0),
-            games_played: parseInt(0),
-            games_won: parseInt(0),
-            time_played: parseInt(0)
-          });
-
-          newUser.save(function(err) {
-            if (err) {
-              throw err;
-            }
-
-            console.log('New user created successfully');
-            res.json({
-              success: true,
-              message: 'New user created.'
+        bcrypt.genSalt(12, function(err, salt) {
+          bcrypt.hash(password, salt, function(err, hash) {
+            var newUser = new User({
+              name: name,
+              password: hash,
+              admin: false,
+              shots_fired: parseInt(0),
+              kills: parseInt(0),
+              deaths: parseInt(0),
+              games_played: parseInt(0),
+              games_won: parseInt(0),
+              time_played: parseInt(0)
             });
 
+            newUser.save(function(err) {
+              if (err) {
+                throw err;
+              }
+
+              console.log('New user created successfully');
+              res.json({
+                success: true,
+                message: 'New user created.'
+              });
+
+            });
           });
         });
-      });
-    }
-  });
+      }
+    });
+  }
+
+  getAThing();
+
 });
 
 module.exports = router;
